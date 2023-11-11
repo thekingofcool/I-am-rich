be168c6ec0dbcd0fea0c474c585585f95c223340
thekingofcool <offweiboforawhile@proton.me>
2023-11-11 09:25:18 +0800

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by thekingofcool on 2023-11-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
